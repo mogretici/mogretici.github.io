b0509ec64f00b9584f064c2d3a87ee651dfaa2b3
Lütfü ÖĞRETİCİ <56653899+mogretici@users.noreply.github.com>
2022-07-10 22:14:33 +0300

    todoApp

---
+++ todoApp/src/App.js
@@ -0,0 +1,13 @@
+import ToDo from './component/todo'
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+<ToDo/>
+    </div>
+  );
+}
+
+export default App;
